6ae065e97ab2eb70faf1866c21d2655183e333c8
Shu Sugimoto <shu@su.gimo.to>
2016-08-26 21:11:44 +0900

    sync

---
+++ oh-my-zsh/self.plugin.zsh
@@ -0,0 +1,12 @@
+
+
+local _self_plugin_dir="${0:h}"
+
+source "$_self_plugin_dir/cdr.zsh"
+source "$_self_plugin_dir/peco-cdr.zsh"
+source "$_self_plugin_dir/peco-select-history.zsh"
+source "$_self_plugin_dir/peco-misc.zsh"
+source "$_self_plugin_dir/misc.zsh"
+
+unset _self_plugin_dir
+
