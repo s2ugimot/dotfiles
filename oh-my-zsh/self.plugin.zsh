d1eca93a0ae7ec831a9a9a24462cd0d94ec13f34
Shu Sugimoto <shu@su.gimo.to>
2020-02-12 18:57:07 +0900

    Add gconf command

--- oh-my-zsh/self.plugin.zsh
+++ oh-my-zsh/self.plugin.zsh
@@ -6,6 +6,7 @@ source "$_self_plugin_dir/cdr.zsh"
 source "$_self_plugin_dir/peco-cdr.zsh"
 source "$_self_plugin_dir/peco-select-history.zsh"
 source "$_self_plugin_dir/peco-misc.zsh"
+source "$_self_plugin_dir/gconf.zsh"
 source "$_self_plugin_dir/misc.zsh"
 
 unset _self_plugin_dir
