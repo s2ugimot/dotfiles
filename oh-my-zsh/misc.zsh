12377d89dd33f8e7a091364d8a18e6fb25e65cb1
Shu Sugimoto <shu@su.gimo.to>
2020-02-17 09:26:09 +0900

    Add diff-highlight alias

--- oh-my-zsh/misc.zsh
+++ oh-my-zsh/misc.zsh
@@ -1,2 +1,8 @@
+# タブで履歴を共有しない
 unsetopt share_history
+
+# fig
 alias fig=docker-compose
+
+# diff-highlight
+alias diff-highlight=/usr/local/opt/git/share/git-core/contrib/diff-highlight/diff-highlight
